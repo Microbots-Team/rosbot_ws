0ebe490c20eadfe2f8bdaf371885de3b2fdab9b7
Rami Sabbagh <rami.sab07@gmail.com>
2025-03-21 09:39:26 +0300

    Create `shapes-turtles` package

---
+++ src/shapes-turtles/README.md
@@ -0,0 +1,5 @@
+
+## Development Journal
+
+- Create the package using `ros2 pkg create --built-type ament_python --license MIT shapes-turtles`.
+- Set the project version, description, maintainer details in both `package.xml` and `setup.py`.
---
+++ src/shapes-turtles/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'shapes-turtles'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rami',
+    maintainer_email='rami.sab07@gmail.com',
+    description='For drawing primitive shapes in turtlesim.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
